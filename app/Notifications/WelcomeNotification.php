7e8163d6aa4e4d28098392c1f0d3bf1fa43b3a9a
Akintola Oluwaseun Patrick <63754193+PatrickCode1299@users.noreply.github.com>
2024-08-01 12:29:34 +0100

    Broadcast Notification added to program logic..

---
+++ app/Notifications/WelcomeNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    protected $greeting;
+    public function __construct($greeting)
+    {
+        $this->greeting=$greeting;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting($this->greeting['welcome_text'])
+                    ->line($this->greeting['body'])
+                    ->action($this->greeting['call_to_action'], $this->greeting['url'])
+                    ->line($this->greeting['conclusion']);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+          'greeting_id'=>  $this->greeting['id']
+        ];
+    }
+}
