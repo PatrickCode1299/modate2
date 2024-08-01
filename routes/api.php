f6cbc6397437d36c09b6beb6a415baa4cf8bee75
Akintola Oluwaseun Patrick <63754193+PatrickCode1299@users.noreply.github.com>
2024-08-01 12:10:54 +0100

    Routes changes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Broadcast;
+use App\Http\Controllers\WebRTCController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +29,7 @@ Route::post('/profile', [AuthController::class, 'checkIfUserHasCompleteProfile']
 Route::post('/updateProfile', [AuthController::class, 'updateUserProfile']);
 Route::post('/find', [AuthController::class, 'findUserMatch']);
 Route::post('/choice', [AuthController::class, 'setUserMatch']);
+Route::post('/removeChoice', [AuthController::class, 'unMatchUser']);
 Route::post('/update',[AuthController::class, 'updateUserPicture']);
 Route::post('/updateCoverPhoto',[AuthController::class, 'updateCoverPhoto']);
 Route::post('/createUserPost',[AuthController::class, 'createUserPost']);
@@ -57,6 +60,59 @@ Route::post('/findAllComments',[AuthController::class, 'findAllComments']);
 Route::post('/DeleteComment',[AuthController::class, 'DeleteComment']);
 Route::post('/findNotifications',[AuthController::class, 'findNotifications']);
 Route::post('/findNotifyCount',[AuthController::class, 'findNotifyCount']);
+Route::post('/convo',[AuthController::class, 'convo']);
+Route::post('/findConvo',[AuthController::class, 'findConvo']);
+Route::post('/sendMessage',[AuthController::class, 'sendMessage']);
+Route::post('/findAllMessages',[AuthController::class, 'findAllMessages']);
+Route::post('/findRecieverInfo',[AuthController::class, 'findRecieverInfo']);
+Route::post('/updateMsgStatus',[AuthController::class, 'updateMsgStatus']);
+Route::post('/findMsgCount',[AuthController::class, 'findMsgCount']);
+Route::post('/createUserChannelVideo',[AuthController::class, 'createUserChannelVideo']);
+Route::post('/replyComment',[AuthController::class, 'replyComment']);
+Route::post('/findCommentReplies',[AuthController::class, 'findCommentReplies']);
+Route::post('/DeleteCommentReply',[AuthController::class, 'DeleteCommentReply']);
+Route::post('/findAllChannelPhotos',[AuthController::class, 'findAllChannelPhotos']);
+Route::post('/findAllChannelVideos',[AuthController::class, 'findAllChannelVideos']);
+Route::post('/sharePost',[AuthController::class, 'sharePost']);
+Route::post('/fetchAllSharedPost',[AuthController::class, 'fetchAllSharedPost']);
+Route::post('/findUserPost',[AuthController::class, 'findUserPost']);
+Route::post('/blockUser',[AuthController::class, 'blockUser']);
+Route::post('/isUserBlocked',[AuthController::class, 'check_if_current_user_has_being_blocked']);
+Route::post('/isUserFollowed',[AuthController::class, 'check_if_current_user_has_being_followed']);
+Route::post('/bookmarkPost',[AuthController::class, 'bookmarkPost']);
+Route::post('/findAllBookMarkedPosts',[AuthController::class, 'findAllBookMarkedPosts']);
+Route::post('/Search',[AuthController::class, 'Search']);
+Route::post('/deleteUserPost',[AuthController::class, 'deleteUserPost']);
+Route::post('/findAllUserReply',[AuthController::class, 'findAllUserReply']);
+Route::post('/findSharedStatus',[AuthController::class, 'findSharedStatus']);
+Route::post('/disableUserComment',[AuthController::class, 'disableUserComment']);
+Route::post('/findFollowersCount',[AuthController::class, 'find_user_followers']);
+Route::post('/suggestUsers',        [AuthController::class, 'suggestUsers']);
+Route::post('/generate-link', [WebRTCController::class, 'generateLink']);
+Route::post('/broadcast-signal', [WebRTCController::class, 'broadcastSignal']);
+Route::post('/signalPusher', [WebRTCController::class, 'signalPusher']);
+Route::delete('/delete-stream-videos/{streamId}', [WebRTCController::class, 'deleteStreamVideos']);
+Route::post('/live-video/chunk', [WebRTCController::class, 'getVideoChunk']);
+Route::post('/uploadTextStory', [AuthController::class, 'uploadTextStory']);
+Route::post('/findComment', [AuthController::class, 'findComment']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
